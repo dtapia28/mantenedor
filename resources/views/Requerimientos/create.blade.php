bcd2efc3822139f51f38825e8bec4132f60995a1
dtapia28 <dtapia1025@gmail.com>
2019-09-09 10:57:46 -0300

    Se borra script de create requerimiento

--- resources/views/Requerimientos/create.blade.php
+++ resources/views/Requerimientos/create.blade.php
@@ -87,14 +87,4 @@
     <p>
         <a href="../requerimientos">Regresar al listado de requerimientos</a>
     </p>
-   <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>   
-    <script type="text/javascript">
-        $(document).ready(function(){
-            setInterval(
-                function(){
-                    $('#recargar').load('{{ asset('js/resolutores.php') }}');
-                }, 2000
-            );
-        });
-    </script>
 @endsection
