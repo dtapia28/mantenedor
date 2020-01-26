356239b9cd217ce67ac2d1ae93bbf4af58a6ea80
Cesar Ramos <cesar3r2@gmail.com>
2020-01-26 20:19:02 -0300

    Agrega vistas del dashboard para todos los perfiles

--- resources/views/dashboard/administrador.blade.php
+++ resources/views/dashboard/administrador.blade.php
@@ -1,27 +1,202 @@
 <div class="ibox">
-  <div class="ibox-head">
-    <div class="ibox-title">Apilado por equipo</div>
-  </div>
-  <div class="ibox-body" align="center">
-    <div id="" class=""></div>
-  </div>
+	<div class="ibox-body" align="center">
+		<div id="chart-apilado" style="min-height:60vh"></div>
+	</div>
 </div>
 <div class="ibox">
-    <div class="ibox-head">
-      <div class="ibox-title">Medidor general</div>
-    </div>
     <div class="ibox-body" align="center">
-      <div id="" class=""></div>
+		<div id="chart-medidor-gral" style="min-height:40vh"></div>
     </div>
 </div>
 <div class="ibox">
-  <div class="ibox-head">
-    <div class="ibox-title">Medidor por Equipo</div>
-    <div class="ibox-tools">
-      <a class="ibox-collapse"><i class="fa fa-minus"></i></a>
-    </div>
-  </div>
-  <div class="ibox-body" align="center">
-    <div id="" class=""></div>
-  </div>
+	<div class="ibox-head">
+		<div class="ibox-title">Requerimientos por Equipo</div>
+		<div class="ibox-tools">
+			<a class="ibox-collapse"><i class="fa fa-minus"></i></a>
+		</div>
+	</div>
+	<div class="ibox-body" align="center">
+		<div class="row">
+			@for (	$i=0; $i<8; $i++)
+			<div class="col-md-3">
+				<div id="chart-medidor-eq{{$i}}" style="min-height:30vh;"></div>
+			</div>
+			@endfor
+		</div>
+	</div>
 </div>
+
+@section('scripts_dash')
+	<script type="text/javascript">
+		const equipos = ["Matzikama", "Cederberg", "Bergrivier", "Swartland", "Witzenberg", "Langeberg", "George", "Laingsburg"];
+		const vencidos = ["357", "267", "555", "679", "238", "852", "357", "855"];
+		const alDia = ["356", "473", "635", "244", "588", "870", "883", "679"];
+		const porVencer = ["635", "263", "346", "356", "619", "588", "883", "870"];
+		
+		var nombreEquipos = [];
+		equipos.forEach(element => {
+			var obj = {};
+			obj.label = element;
+			nombreEquipos.push(obj);
+			return nombreEquipos;
+		});
+
+		var valoresVencidos = [];
+		vencidos.forEach(element => {
+			var obj = {};
+			obj.value = element;
+			valoresVencidos.push(obj);
+			return valoresVencidos;
+		});
+
+		var valoresAlDia = [];
+		alDia.forEach(element => {
+			var obj = {};
+			obj.value = element;
+			valoresAlDia.push(obj);
+			return valoresAlDia;
+		});
+
+		var valoresPorVencer = [];
+		porVencer.forEach(element => {
+			var obj = {};
+			obj.value = element;
+			valoresPorVencer.push(obj);
+			return valoresPorVencer;
+		});
+		
+		$("#chart-apilado").insertFusionCharts({
+			type: "stackedcolumn3d",
+			width: "100%",
+			height: "100%",
+			dataFormat: "json",
+			dataSource: {
+				chart: {
+					caption: "Requerimientos por Equipo",
+					subcaption: "Por Estatus",
+					numvisibleplot: "6",
+					showvalues: "1",
+					decimals: "0",
+					// stack100percent: "1",
+					valuefontcolor: "#FFFFFF",
+					exportEnabled: "1",
+					plottooltext:
+						"$label tiene $dataValue $seriesName",
+					theme: "fusion"
+				},
+				categories: [
+				{
+					category: nombreEquipos
+				}
+				],
+				dataset: [
+				{
+					seriesname: "Vencidos",
+					data: valoresVencidos,
+					color: "#e74c3c",
+				},
+				{
+					seriesname: "Por Vencer",
+					data: valoresPorVencer,
+					color: "#f39c12",
+				},
+				{
+					seriesname: "Al día",
+					data: valoresAlDia,
+					color: "#2ecc71",
+				}
+				]
+			}
+		});
+
+		$("#chart-medidor-gral").insertFusionCharts({
+			type: "angulargauge",
+			width: "100%",
+			height: "100%",
+			dataFormat: "json",
+			dataSource: {
+				chart: {
+					caption: "Requerimientos en General",
+					lowerlimit: "0",
+					upperlimit: "100",
+					showvalue: "1",
+					// numbersuffix: "%",
+					theme: "fusion",
+					exportEnabled: "1",
+					showtooltip: "0",
+				},
+				colorrange: {
+				color: [
+					{
+					minvalue: "0",
+					maxvalue: "50",
+					code: "#F2726F"
+					},
+					{
+					minvalue: "50",
+					maxvalue: "75",
+					code: "#FFC533"
+					},
+					{
+					minvalue: "75",
+					maxvalue: "100",
+					code: "#62B58F"
+					}
+				]
+				},
+				dials: {
+				dial: [
+					{
+					value: "62"
+					}
+				]
+				}
+			}
+		});
+
+		for(var i=0; i<8; i++) {
+			$("#chart-medidor-eq"+i).insertFusionCharts({
+				type: "angulargauge",
+				width: "100%",
+				height: "100%",
+				dataFormat: "json",
+				dataSource: {
+					chart: {
+						caption: equipos[i],
+						lowerlimit: "0",
+						upperlimit: "100",
+						showvalue: "1",
+						theme: "fusion",
+						showtooltip: "0",
+					},
+					colorrange: {
+					color: [
+						{
+						minvalue: "0",
+						maxvalue: "50",
+						code: "#F2726F"
+						},
+						{
+						minvalue: "50",
+						maxvalue: "75",
+						code: "#FFC533"
+						},
+						{
+						minvalue: "75",
+						maxvalue: "100",
+						code: "#62B58F"
+						}
+					]
+					},
+					dials: {
+					dial: [
+						{
+						value: "62"
+						}
+					]
+					}
+				}
+			});
+		}
+    </script>
+@endsection
--- resources/views/dashboard/solicitante.blade.php
+++ resources/views/dashboard/solicitante.blade.php
@@ -1,17 +1,132 @@
 <div class="ibox">
-    <div class="ibox-head">
-      <div class="ibox-title">Gráfico circular</div>
-    </div>
     <div class="ibox-body" align="center">
-      <div id="" class=""></div>
+		<div id="chart-dona" style="min-height:50vh"></div>
     </div>
 </div>
 
 <div class="ibox">
-  <div class="ibox-head">
-    <div class="ibox-title">Gráfico apilado por equipo</div>
-  </div>
   <div class="ibox-body" align="center">
-    <div id="" class=""></div>
+    <div id="chart-apilado" style="min-height:60vh"></div>
   </div>
 </div>
+
+@section('scripts_dash')
+	<script type="text/javascript">
+		$("#chart-dona").insertFusionCharts({
+			type: "doughnut3d",
+			width: "100%",
+			height: "100%",
+			dataFormat: "json",
+			dataSource: {
+				chart: {
+					caption: "Requerimientos por Estatus",
+					enablesmartlabels: "1",
+					showlabels: "1",
+					numbersuffix: " MMbbl",
+					exportEnabled: "1",
+					usedataplotcolorforlabels: "1",
+					plottooltext: "$label: <b>$value</b>",
+					theme: "fusion"
+				},	
+				data: [
+				{
+					label: "Al Día",
+					value: "290",
+					color: "#2ecc71",
+				},
+				{
+					label: "Por Vencer",
+					value: "260",
+					color: "#f39c12",
+				},
+				{
+					label: "Vencidos",
+					value: "180",
+					color: "#e74c3c",
+				}
+				]
+			}
+		});
+
+		const equipos = ["Matzikama", "Cederberg", "Bergrivier", "Swartland", "Witzenberg", "Langeberg", "George", "Hessequa", "Laingsburg"];
+		const vencidos = ["357", "267", "555", "679", "138", "852", "357", "664", "855"];
+		const alDia = ["356", "173", "635", "144", "588", "870", "883", "306", "679"];
+		const porVencer = ["635", "263", "346", "356", "619", "588", "174", "883", "870"];
+		
+		var nombreEquipos = [];
+		equipos.forEach(element => {
+			var obj = {};
+			obj.label = element;
+			nombreEquipos.push(obj);
+			return nombreEquipos;
+		});
+
+		var valoresVencidos = [];
+		vencidos.forEach(element => {
+			var obj = {};
+			obj.value = element;
+			valoresVencidos.push(obj);
+			return valoresVencidos;
+		});
+
+		var valoresAlDia = [];
+		alDia.forEach(element => {
+			var obj = {};
+			obj.value = element;
+			valoresAlDia.push(obj);
+			return valoresAlDia;
+		});
+
+		var valoresPorVencer = [];
+		porVencer.forEach(element => {
+			var obj = {};
+			obj.value = element;
+			valoresPorVencer.push(obj);
+			return valoresPorVencer;
+		});
+		
+		$("#chart-apilado").insertFusionCharts({
+			type: "stackedcolumn3d",
+			width: "100%",
+			height: "100%",
+			dataFormat: "json",
+			dataSource: {
+				chart: {
+					caption: "Requerimientos por Equipo",
+					subcaption: "Por Estatus",
+					numvisibleplot: "6",
+					showvalues: "1",
+					decimals: "0",
+					// stack100percent: "1",
+					valuefontcolor: "#FFFFFF",
+					exportEnabled: "1",
+					plottooltext:
+						"$label tiene $dataValue $seriesName",
+					theme: "fusion"
+				},
+				categories: [
+				{
+					category: nombreEquipos
+				}
+				],
+				dataset: [
+				{
+					seriesname: "Vencidos",
+					data: valoresVencidos,
+					color: "#e74c3c",
+				},
+				{
+					seriesname: "Por Vencer",
+					data: valoresPorVencer,
+					color: "#f39c12",
+				},
+				{
+					seriesname: "Al día",
+					data: valoresAlDia,
+					color: "#2ecc71",
+				}
+				]
+			}
+		});
+    </script>
+@endsection
