420fa7972491e47a3621652ec74521324f742c87
César Ramos <cesar3r2@gmail.com>
2020-11-15 21:18:15 -0300

    Ajustes al reporte semanal

--- resources/views/emails/envia_reporte_semanal.blade.php
+++ resources/views/emails/envia_reporte_semanal.blade.php
@@ -4,7 +4,7 @@
         <a href="https://app.kinchika.com/login"><img src="{{ asset('img/logo-blue.png') }}" alt="logo"></a>
     </div>
     <div class="ibox-head">
-        <div class="ibox-title"><h1>Reporte Requerimientos</h1></div>       
+        <div class="ibox-title" align="center"><h1>Reporte Requerimientos</h1></div>       
     </div>
     <div class="ibox-body">
         <style>
@@ -15,7 +15,7 @@
             .head_table{
                 width: 120rem;
                 font-size: 2.5rem;
-                text-align: center;
+                text-align: left;
             }
             
             tbody th{
@@ -24,68 +24,83 @@
             }
             table,td {
                 border: 1px solid black;
-            }           
+            }
+            .aj-auto {
+                width: auto;
+            }
+            .bg-verde {
+                background-color: #28a745;
+            }
+            .bg-rojo {
+                background-color: #ff3333;
+            }
+            .bg-morado {
+                background-color: #7733ff;
+            }
+            .bg-amarillo {
+                background-color: #fcdf00;
+            }
         </style>
-        <table width=80%>
+        <table align="center" cellpadding="5" cellspacing="5" style="font-family: Arial, Helvetica, sans-serif" border="0">
             <tr >
                 <td class="head_table"><h2>Requerimientos al {{ $hoy }}</h2></td>
             </tr>
-            <table border="2" width=80%>
+            <table border="0" style="border: 1px solid #343a40;" cellpadding="5" cellspacing="0">
                 <thead>
-                    <tr>
-                        <th width=10%>Area</th>
-                        <th width=20%>Resolutor</th>
-                        <th width=10%>Cantidad de RQ activos al {{$ayer}}</th>
-                        <th width=10%>Vencidos</th>
-                        <th width=10%>Cantidad de RQ generado el dia de hoy {{ $hoy }}</th>
-                        <th width=10%>Cantidad de RQ cerrado el  dia de hoy {{ $hoy }}</th>
-                        <th width=10%>Cantidad de RQ activos al {{$hoy}}</th>
-                        <th width=10%>Color segun cant de RQ vencidos</th>
+                    <tr style="color: #f8f9fa; background-color: #343a40; border: 1px solid #f8f9fa;">
+                        <th class="aj-auto">Área</th>
+                        <th class="aj-auto">Resolutor</th>
+                        <th class="aj-auto">Cant. de RQ activos<br>al {{$ayer}}</th>
+                        <th class="aj-auto">Vencidos</th>
+                        <th class="aj-auto">Cant. de RQ generados<br>el día de hoy {{ $hoy }}</th>
+                        <th class="aj-auto">Cant. de RQ cerrados<br>el día de hoy {{ $hoy }}</th>
+                        <th class="aj-auto">Cant. de RQ activos <br>al {{$hoy}}</th>
+                        <th class="aj-auto">Color según cant.<br>de RQ vencidos</th>
                     </tr>
                 </thead>
                 <tbody>
                     @for ($i=0; $i<count((array)$valores["resolutores_array"]); $i++)
                             <tr>
-                                <td>{{$valores['equipos_array'][$i]}}</td>
-                                <td>{{$valores['resolutores_array'][$i]}}</td>
-                                <td style="text-align: center;">{{$valores['pendientes_resolutor_hoy'][$i]+$valores['cerrados'][$i]-$valores['creadoHoy_resolutor'][$i]}}</td>
+                                <td style="border: 1px solid #343a40;">{{$valores['equipos_array'][$i]}}</td>
+                                <td style="border: 1px solid #343a40;">{{$valores['resolutores_array'][$i]}}</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">{{$valores['pendientes_resolutor_hoy'][$i]+$valores['cerrados'][$i]-$valores['creadoHoy_resolutor'][$i]}}</td>
                                 @if ($valores['vencidos'][$i] >= 1)
-                                <td style="text-align: center; color: red; font-weight: bold;">{{$valores['vencidos'][$i]}}</td>
+                                <td style="text-align: center; color: red; font-weight: bold; border: 1px solid #343a40;">{{$valores['vencidos'][$i]}}</td>
                                 @else
-                                <td style="text-align: center;">{{$valores['vencidos'][$i]}}</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">{{$valores['vencidos'][$i]}}</td>
                                 @endif
-                                <td style="text-align: center;">{{$valores['creadoHoy_resolutor'][$i]}}</td>
-                                <td style="text-align: center;">{{$valores['cerrados'][$i]}}</td>
-                                <td style="text-align: center;">{{$valores['pendientes_resolutor_hoy'][$i]}}</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">{{$valores['creadoHoy_resolutor'][$i]}}</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">{{$valores['cerrados'][$i]}}</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">{{$valores['pendientes_resolutor_hoy'][$i]}}</td>
                                 @if ($valores['vencidos'][$i] >= 1 and $valores['vencidos'][$i]<=3)
-                                <td style="background-color: yellow"></td>
+                                <td class="bg-amarillo" style="border: 1px solid #343a40;"></td>
                                 @elseif($valores['vencidos'][$i]>=4 and $valores['vencidos'][$i]<=6)
-                                <td style="background-color: red"></td>
+                                <td class="bg-rojo" style="background-color: #dc3545; border: 1px solid #343a40;"></td>
                                 @elseif($valores['vencidos'][$i]>=7)
-                                <td style="background-color: #45046a; font-weight: bold; color: white; text-align: center;">GRAVE</td>
+                                <td class="bg-morado" style="font-weight: bold; color: white; text-align: center; border: 1px solid #343a40;">GRAVE</td>
                                 @else
-                                <td style="background-color: green"></td>
+                                <td class="bg-verde" style="border: 1px solid #343a40;"></td>
                                 @endif                               
                             </tr>
                     @endfor
                     <tr>
-                        <td colspan="2" style="font-weight: bold;">Total RQ</td>
-                        <td style="font-weight: bold; text-align: center;">{{$valores['total_activos_ayer']}}</td>
-                        <td style="color: red; font-weight: bold; text-align: center;">{{$valores['total_vencidos']}}</td>
-                        <td style="font-weight: bold; text-align: center;">{{$valores['total_creados_hoy']}}</td>
-                        <td style="font-weight: bold; text-align: center;">{{$valores['total_cerrados_hoy']}}</td>
-                        <td style="font-weight: bold; text-align: center;">{{$valores['total_activos_hoy']}}</td>
+                        <td colspan="2" style="font-weight: bold; border: 1px solid #343a40;">Total RQ</td>
+                        <td style="font-weight: bold; text-align: center; border: 1px solid #343a40;">{{$valores['total_activos_ayer']}}</td>
+                        <td style="color: red; font-weight: bold; text-align: center; border: 1px solid #343a40;">{{$valores['total_vencidos']}}</td>
+                        <td style="font-weight: bold; text-align: center; border: 1px solid #343a40;">{{$valores['total_creados_hoy']}}</td>
+                        <td style="font-weight: bold; text-align: center; border: 1px solid #343a40;">{{$valores['total_cerrados_hoy']}}</td>
+                        <td style="font-weight: bold; text-align: center; border: 1px solid #343a40;">{{$valores['total_activos_hoy']}}</td>
                     </tr>
                 </tbody>
             </table>
         </table>
         <br>
-        <table border='1'>
+        <table border="0" width="100%" style="border: 1px solid #343a40;" cellpadding="5" cellspacing="0">
             <thead>
-                <tr>
-                    <th>Semaforo</th>
+                <tr style="color: #f8f9fa; background-color: #343a40; border: 1px solid #343a40;">
+                    <th class="aj-auto">Semáforo</th>
                     @foreach($valores["dias"] as $dia)
-                    <th>{{$dia}}</th>
+                    <th class="aj-auto">{{$dia}}</th>
                     @endforeach
                 </tr>
             </thead>
@@ -94,37 +109,37 @@
             @foreach($valores['dias'] as $dia)
             @if($valores['dias'][0] == $dia)
             <tr>
-                    <td>{{$valores['resolutores_array'][$i]}}</td>
+                <td class="aj-auto" style="border: 1px solid #343a40; white-space: nowrap;">{{$valores['resolutores_array'][$i]}}</td>
             @endif
-                    @foreach($valores['todo'] as $elemento)
+                @foreach($valores['todo'] as $elemento)
                     @foreach($valores['array_id_resolutor'] as $res)
-                    @if($elemento->fecha == $dia and $valores['resolutores_array'][$i] == $res[0]->nombreResolutor and $res[0]->id == $elemento->id_resolutor)
-                                @if ($elemento->nota >= 1 and $elemento->nota <=3)
-                                <td style="background-color: yellow"></td>
-                                @elseif($elemento->nota >=4 and $elemento->nota <=6)
-                                <td style="background-color: red"></td>
-                                @elseif($elemento->nota >=7)
-                                <td style="background-color: #45046a; font-weight: bold; color: white; text-align: center;">GRAVE</td>
-                                @else
-                                <td style="background-color: green"></td>
-                                @endif
-                    @endif
+                        @if($elemento->fecha == $dia and $valores['resolutores_array'][$i] == $res[0]->nombreResolutor and $res[0]->id == $elemento->id_resolutor)
+                            @if ($elemento->nota >= 1 and $elemento->nota <=3)
+                                <td class="bg-amarillo" style="border: 1px solid #343a40;"></td>
+                            @elseif($elemento->nota >=4 and $elemento->nota <=6)
+                                <td class="bg-rojo" style="border: 1px solid #343a40;"></td> 
+                            @elseif($elemento->nota >=7)
+                                <td class="bg-morado" style="font-weight: bold; color: white; text-align: center; border: 1px solid #343a40;">GRAVE</td>
+                            @else
+                                <td class="bg-verde" style="border: 1px solid #343a40;"></td>
+                            @endif
+                         @endif
                     @endforeach
-                    @endforeach
+                @endforeach
             @endforeach
             </tr>
             @endfor
             </tbody>
         </table>
         <br>
-        <table border='1'>
+        <table border="0" width="100%" style="border: 1px solid #343a40;" cellpadding="5" cellspacing="0">
             <thead>
-                <tr>
-                    <th>Nota</th>
+                <tr style="color: #f8f9fa; background-color: #343a40; border: 1px solid #f8f9fa;">
+                    <th class="aj-auto">Nota</th>
                     @foreach($valores["dias"] as $dia)
-                    <th>{{$dia}}</th>
+                    <th class="aj-auto">{{$dia}}</th>
                     @endforeach
-                    <th style="text-align: center">Promedio</th>
+                    <th class="aj-auto" style="text-align: center">Promedio</th>
                 </tr>
             </thead>
             <tbody>
@@ -132,25 +147,25 @@
             @foreach($valores['dias'] as $dia)
             @if($valores['dias'][0] == $dia)
             <tr>
-                    <td>{{$valores['resolutores_array'][$i]}}</td>
+                    <td class="aj-auto" style="border: 1px solid #343a40; white-space: nowrap">{{$valores['resolutores_array'][$i]}}</td>
             @endif
                     @foreach($valores['todo'] as $elemento)
                     @foreach($valores['array_id_resolutor'] as $res)
                     @if($elemento->fecha == $dia and $valores['resolutores_array'][$i] == $res[0]->nombreResolutor and $res[0]->id == $elemento->id_resolutor)
                                 @if ($elemento->nota >= 1 and $elemento->nota <=3)
-                                <td style="text-align: center">6</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">6</td>
                                 @elseif($elemento->nota >=4 and $elemento->nota <=6)
-                                <td style="text-align: center">4</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">4</td>
                                 @elseif($elemento->nota >=7)
-                                <td style="text-align: center">1</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">1</td>
                                 @else
-                                <td style="text-align: center">10</td>
+                                <td style="text-align: center; border: 1px solid #343a40;">10</td>
                                 @endif
                     @endif
                     @endforeach
                     @endforeach
             @endforeach
-            <td style="text-align: center; font-weight: bold;">{{$valores['totales_notas'][$i]}}</td>
+            <td style="text-align: center; font-weight: bold; border: 1px solid #343a40;">{{$valores['totales_notas'][$i]}}</td>
             </tr>
             @endfor
             </tbody>            
